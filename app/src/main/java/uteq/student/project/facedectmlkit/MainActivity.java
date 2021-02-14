c79e442daefbc7d6c1423a08ebed5f1b63131cdd
lsinchiguanos <leslie.sinchiguano2015@uteq.edu.ec>
2021-02-14 04:24:15 -0500

    Codigo y permisos

--- app/src/main/java/uteq/student/project/facedectmlkit/MainActivity.java
+++ app/src/main/java/uteq/student/project/facedectmlkit/MainActivity.java
@@ -1,14 +1,157 @@
 package uteq.student.project.facedectmlkit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+import com.wonderkiln.camerakit.CameraKitError;
+import com.wonderkiln.camerakit.CameraKitEvent;
+import com.wonderkiln.camerakit.CameraKitEventListener;
+import com.wonderkiln.camerakit.CameraKitImage;
+import com.wonderkiln.camerakit.CameraKitVideo;
+import com.wonderkiln.camerakit.CameraView;
+
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
+import uteq.student.project.facedectmlkit.Helpers.GraphicOverlay;
+import uteq.student.project.facedectmlkit.Helpers.RectOverlay;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    CameraView cameraView;
+    Button btnDetector;
+    AlertDialog alertDialog;
+    GraphicOverlay graphicOverlay;
+    FaceDetectorOptions highAccuracyOpts =
+            new FaceDetectorOptions.Builder()
+                    .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                    .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+                    .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+                    .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cameraView = findViewById(R.id.camera);
+        btnDetector = findViewById(R.id.detect);
+        btnDetector.setOnClickListener(this);
+        graphicOverlay = findViewById(R.id.graphic_overlay);
+        cameraView.addCameraKitListener(new CameraKitEventListener() {
+            @Override
+            public void onEvent(CameraKitEvent cameraKitEvent) {
+
+            }
+
+            @Override
+            public void onError(CameraKitError cameraKitError) {
+
+            }
+
+            @Override
+            public void onImage(CameraKitImage cameraKitImage) {
+                showAlertDialogWithAutoDismiss();
+                Bitmap bitmap = cameraKitImage.getBitmap();
+                bitmap = Bitmap.createScaledBitmap(bitmap, cameraView.getWidth(), cameraView.getHeight(), false);
+                cameraView.stop();
+            }
+
+            @Override
+            public void onVideo(CameraKitVideo cameraKitVideo) {
+
+            }
+        });
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.detect:
+                cameraView.start();
+                cameraView.captureImage();
+                graphicOverlay.clear();
+                break;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        cameraView.stop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        cameraView.stop();
+    }
+
+    public void showAlertDialogWithAutoDismiss(){
+        alertDialog = new SpotsDialog.Builder().setContext(this).setMessage("Espere, Cargando...").setCancelable(false).build();
+        alertDialog.show();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (alertDialog.isShowing()){
+                    alertDialog.dismiss();
+                    Toast.makeText(MainActivity.this, "Rostro no detectado",Toast.LENGTH_LONG).show();
+                    cameraView.start();
+                }
+            }
+        }, 5000);
+    }
+
+    private void detectFace(Bitmap bitmap){
+        int rotationDegree = 0;
+        InputImage image = InputImage.fromBitmap(bitmap, rotationDegree);
+        FaceDetector detector = FaceDetection.getClient(highAccuracyOpts);
+        detector.process(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<List<Face>>() {
+                            @Override
+                            public void onSuccess(List<Face> faces) {
+                                drawFace(faces);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "ERROR",Toast.LENGTH_SHORT).show();
+                            }
+                        });
+    }
+
+    private void drawFace(List<Face> faces) {
+        for (Face face: faces){
+            Rect rect = face.getBoundingBox();
+            RectOverlay rectOverlay = new RectOverlay(graphicOverlay, rect);
+            graphicOverlay.add(rectOverlay);
+
+            alertDialog.dismiss();
+        }
+    }
+
 }
